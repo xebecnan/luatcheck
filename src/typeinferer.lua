9b46b72362dc5ee33c050347c40a14332d916daf
arnan <shenglunan@gmail.com>
2023-07-04 23:02:46 +0800

    working

--- src/typeinferer.lua
+++ src/typeinferer.lua
@@ -1,3 +1,5 @@
+-- TODO: 避免往 solution 里添加重复条目
+
 local Util = require 'util'
 
 local dump_table = Util.dump_table
@@ -5,7 +7,7 @@ local ast_error = Util.ast_error
 
 local sf = string.format
 
-local function array_copy(s, d)
+local function array_append_to(s, d)
     for _, v in ipairs(s) do
         d[#d+1] = v
     end
@@ -17,10 +19,32 @@ local function table_copy(s, d)
     end
 end
 
+local function array_map_append(s, d, map_func, ...)
+    for _, v in ipairs(s) do
+        d[#d+1] = map_func(v, ...)
+    end
+end
+
+local function is_simple_type(v)
+    return v.tag == 'SimpleType'
+end
+
+local function is_type_var(v)
+    return v.tag == 'TypeVar'
+end
+
+local function is_func_type(v)
+    return v.tag == 'FuncType'
+end
+
+local function is_args_type(v)
+    return v.tag == 'ArgsType'
+end
+
 local is_subtype_of
 
 local simple_type_mt = {
-    __tostring = function(v) return sf('<%s>', v[1]) end,
+    __tostring = function(v) return sf('%s', v[1]) end,
     __index = nil,
     is_equal_to = function(v1, v2, allow_subtype)
         if v2.tag ~= 'SimpleType' then
@@ -37,6 +61,8 @@ local simple_type_mt = {
 
         return false
     end,
+
+    get_parent_type = nil,
 }
 simple_type_mt.__index = simple_type_mt
 
@@ -61,6 +87,10 @@ local parent_def = {
     [t_integer] = t_number,
     [t_float]   = t_number,
 }
+
+simple_type_mt.get_parent_type = function(v)
+    return parent_def[v]
+end
 
 --local TYPE_DEF = {
 --    Any     = { name='any' },
@@ -92,7 +122,7 @@ is_subtype_of = function(t1, t2)
             return true
         end
         -- print(p, 'is not equal to', t2)
-        p = parent_def[p]
+        p = p:get_parent_type()
         -- local v = TYPE_DEF[p] or error(sf('unknown type: %s', p))
         -- p = v.parent
     end
@@ -101,11 +131,13 @@ end
 
 local function is_type_array_equal(t1, t2, allow_subtype)
     if #t1 ~= #t2 then
+        -- print('is_type_array_equal failed. count not match', #t1, #t2)
         return false
     end
 
     for i = 1, #t1 do
-        if not t1[i]:is_equal_to(t2, allow_subtype) then
+        if not t1[i]:is_equal_to(t2[i], allow_subtype) then
+            -- print('is_type_array_equal failed. item not match', i, t1[i], t2[i])
             return false
         end
     end
@@ -122,14 +154,18 @@ local function type_array_tostring(t)
 end
 
 local args_type_mt = {
-    __tostring = function(v) return sf('<Args:%s>', type_array_tostring(v.arg_types)) end,
+    __tostring = function(v) return sf('[%s]', type_array_tostring(v.arg_types)) end,
     __index = nil,
     is_equal_to = function(v1, v2, allow_subtype)
         if v2.tag ~= 'ArgsType' then
+            -- print('ArgsType eq failed: v2.tag:', v2.tag)
             return false
         end
 
         return is_type_array_equal(v1.arg_types, v2.arg_types, allow_subtype)
+    end,
+    get_parent_type = function(v)
+        return t_any
     end,
 }
 args_type_mt.__index = args_type_mt
@@ -139,7 +175,7 @@ local function create_args_type(arg_types)
 end
 
 local func_type_mt = {
-    __tostring = function(v) return sf('<Func:%s:%s>', v.args, v.rets) end,
+    __tostring = function(v) return sf('{%s -> %s}', v.args, v.rets) end,
     __index = nil,
     is_equal_to = function(v1, v2, allow_subtype)
         if v2.tag ~= 'FuncType' then
@@ -153,25 +189,9 @@ local func_type_mt = {
 func_type_mt.__index = func_type_mt
 
 local function create_func_type(args, rets)
-    assert(args.tag == 'ArgsType')
-    assert(rets.tag == 'ArgsType')
+    assert(args.tag == 'ArgsType' or is_type_var(args))
+    assert(rets.tag == 'ArgsType' or is_type_var(rets))
     return setmetatable({tag='FuncType', args=args, rets=rets}, func_type_mt)
-end
-
-local function is_simple_type(v)
-    return v.tag == 'SimpleType'
-end
-
-local function is_type_var(v)
-    return v.tag == 'TypeVar'
-end
-
-local function is_func_type(v)
-    return v.tag == 'FuncType'
-end
-
-local function is_args_type(v)
-    return v.tag == 'ArgsType'
 end
 
 --local function is_type_equal(t1, t2)
@@ -194,7 +214,11 @@ end
 
 local type_var_mt = {
     __tostring = function(v)
-        return sf('<TypeVar_%s>', v.type_id)
+        if v.st == t_any then
+            return sf('t%s', v.type_id)
+        else
+            return sf('t%s(%s)', v.type_id, v.st)
+        end
     end,
     __index = nil,
 
@@ -205,6 +229,18 @@ local type_var_mt = {
 
         return v1.type_id and v1.type_id == v2.type_id or false
     end,
+
+    narrow = function(v, target_type)
+        if target_type:is_equal_to(v.st) then
+            return true
+        end
+        if is_subtype_of(target_type, v.st) then
+            v.st = target_type
+            return true
+        end
+        print('!!!!!!!!!!!! narrow failed !!!!!!!!!!!!', target_type, v.st)
+        return false
+    end,
 }
 
 type_var_mt.__index = type_var_mt
@@ -214,7 +250,8 @@ local latest_id = 0
 local function create_type_variable()
     latest_id = latest_id + 1
     return setmetatable(
-        { tag='TypeVar', type_id=latest_id },
+        -- st: supposed type
+        { tag='TypeVar', type_id=latest_id, st=t_any },
         type_var_mt
         )
 end
@@ -226,7 +263,9 @@ local function add_solution(ss, type_var, t)
     --     t = t.arg_types[1]
     -- end
 
+    assert(is_type_var(type_var))
     ss[#ss+1] = { type_var, t }  -- type_var -> t
+    print(sf('    (S) %s => %s', type_var, t))
 end
 
 local function unification_step(v, h, s, ss)
@@ -238,7 +277,7 @@ local function unification_step(v, h, s, ss)
         print('SKIP:', t1, t2)
         return nil
     else
-        print('is equal or subtype to', t1, t2, '----> false')
+        print(sf('"%s" is not equal or subtype to "%s"', t1, t2))
     end
 
     --if is_subtype_of(t1, t2) then
@@ -248,13 +287,15 @@ local function unification_step(v, h, s, ss)
 
     if is_simple_type(t1) and is_type_var(t2) then
         -- s[#s+1] = { t2, t1 }
-        ss[#ss+1] = { t2, t1 }  -- t2 -> t1
+        -- ss[#ss+1] = { t2, t1 }  -- t2 -> t1
+        add_solution(ss, t2, t1)
         return nil
     end
 
     if is_type_var(t1) and is_simple_type(t2) then
         -- s[#s+1] = { t1, t2 }
-        ss[#ss+1] = { t1, t2 }  -- t1 -> t2
+        -- ss[#ss+1] = { t1, t2 }  -- t1 -> t2
+        add_solution(ss, t1, t2)
         return nil
     end
 
@@ -271,6 +312,8 @@ local function unification_step(v, h, s, ss)
     if is_func_type(t1) and is_func_type(t2) then
         h[#h+1] = { t1.args, t2.args }
         h[#h+1] = { t1.rets, t2.rets }
+        print(sf('  [C] %s == %s', t1.args, t2.args))
+        print(sf('  [C] %s == %s', t1.rets, t2.rets))
         return nil
     end
 
@@ -278,6 +321,7 @@ local function unification_step(v, h, s, ss)
         local n = math.max(#t1.arg_types, #t2.arg_types)
         for i = 1, n do
             h[#h+1] = { t1.arg_types[i] or t_any, t2.arg_types[i] or t_any }
+            print(sf('  [C] %s == %s', t1.arg_types[i], t2.arg_types[i]))
         end
         return nil
     end
@@ -290,9 +334,24 @@ end
 
 local function substitute_aux(v, sub)
     local fr, to = sub[1], sub[2]
+    -- print('substitute_aux', v, '|', fr, to)
+    assert(is_type_var(fr))
+
+    if is_func_type(v) then
+        return create_func_type(substitute_aux(v.args, sub), substitute_aux(v.rets, sub))
+    end
+
+    if is_args_type(v) then
+        local h = {}
+        array_map_append(v.arg_types, h, substitute_aux, sub)
+        return create_args_type(h)
+    end
+
     -- FIXME: 要不要用 type equal 来判断相等?
     if v == fr then
-        return to
+        -- print('NARROW', v, to)
+        v:narrow(to)
+        return v
     else
         return v
     end
@@ -308,7 +367,7 @@ end
 local function do_unificaion(c, s)
     local unification_error = false
     local h = {}
-    array_copy(c, h)
+    array_append_to(c, h)
 
     print('-- start unification ------------------------')
     local ss = {}
@@ -416,24 +475,26 @@ function F.If(ast, env, C)
     -- 创建一个未知类型 作为 if 语句的返回类型
     local t = create_type_variable()
     print('IF:', t)
-    local c = {}
     for i = 1, #ast, 2 do
-        -- 约束: 条件的类型必须为 bool
-        local cond_type = inferer_func(ast[i], env, c)
-        c[#c+1] = { cond_type, t_any }
+        -- 约束: 条件的类型必须为 any
+        local cond_type = inferer_func(ast[i], env, C)
+        C[#C+1] = { cond_type, t_any }
+        print(sf('  [C] %s == %s', cond_type, t_any))
 
         -- 约束: 分支的类型必须与 t 相同
-        local body_type = inferer_func(ast[i+1], env, c)
-        c[#c+1] = { body_type, t }
+        local body_type = inferer_func(ast[i+1], env, C)
+        C[#C+1] = { body_type, t }
+        print(sf('  [C] %s == %s', t, body_type))
     end
 
-    local tt, err = resolve(t, c)
-    if not tt then
-        ast_error(ast, err)
-    end
-    print('*********************************************')
-    print('Resolve of IF:', tt)
-    return tt
+    -- local tt, err = resolve(t, c)
+    -- if not tt then
+    --     ast_error(ast, err)
+    -- end
+    -- print('*********************************************')
+    -- print('Resolve of IF:', tt)
+    -- return tt
+    return t
 
     -- local s = {}
     -- local err = do_unificaion(c, s)
@@ -471,9 +532,8 @@ function F.BinOpr(ast, env, C)
     local t = create_type_variable()
     print(sf('BinOpr %s: %s', op, t))
 
-    local c = {}
-    local t1 = inferer_func(n1, env, c)
-    local t2 = inferer_func(n2, env, c)
+    local t1 = inferer_func(n1, env, C)
+    local t2 = inferer_func(n2, env, C)
 
     local args1 = create_args_type{ t1, t2 }
     local rets1 = create_args_type{ t }
@@ -483,15 +543,17 @@ function F.BinOpr(ast, env, C)
 
     -- application (+) t1 t2
     -- C: (type of '+') == (args(t1 t2) -> args(t))
-    c[#c+1] = { func1, func2 }
+    C[#C+1] = { func1, func2 }
+    print(sf('  [C] %s == %s', func1, func2))
 
-    local tt, err = resolve(t, c)
-    if not tt then
-        ast_error(ast, err)
-    end
-    print('*********************************************')
-    print(sf('Resolve of BinOpr(%s): %s', op, tt))
-    return tt
+    -- local tt, err = resolve(t, c)
+    -- if not tt then
+    --     ast_error(ast, err)
+    -- end
+    -- print('*********************************************')
+    -- print(sf('Resolve of BinOpr(%s): %s', op, tt))
+    -- return tt
+    return t
 
     -- if number_binopr_map[op] then
     --     local t = create_type_variable()
@@ -581,15 +643,18 @@ function F.Function(ast, env, C)
     local args = create_args_type(h)
     -- local rets = create_args_type{ inferer_func(n_block, new_env, c) }
     local rets = inferer_func(n_block, new_env, c)
-    local func = create_func_type(args, rets)
+    array_append_to(c, C)
 
-    local tt, err = resolve(func, c)
-    if not tt then
-        ast_error(ast, err)
-    end
-    print('*********************************************')
-    print('Resolve of Function:', tt)
-    return tt
+    local t = create_func_type(args, rets)
+
+    -- local tt, err = resolve(t, c)
+    -- if not tt then
+    --     ast_error(ast, err)
+    -- end
+    -- print('*********************************************')
+    -- print('Resolve of Function:', tt)
+    -- return tt
+    return t
 end
 
 local function reg_number_binopr(env)
@@ -624,5 +689,12 @@ return function(ast)
     reg_buildtin(env)
 
     local c = {}
-    inferer_func(ast, env, c)
+    local t = inferer_func(ast, env, c)
+    local tt, err = resolve(t, c)
+    if not tt then
+        ast_error(ast, err)
+    end
+    print('*********************************************')
+    print('|| typeinferer resolve ||:', tt)
+    return tt
 end
